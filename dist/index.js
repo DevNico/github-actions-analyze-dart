a1c9cadf935ddf263beec759b9c3f7612542c4bb
Nicolas Schlecker <nicolasschlecker@gmail.com>
2021-09-18 15:45:47 +0200

    feat: use dart format and dart analyze

--- dist/index.js
+++ dist/index.js
@@ -1561,119 +1561,108 @@ const fs = __webpack_require__(747);
 const path = __webpack_require__(622);
 
 async function run() {
-  try {
-    const workingDirectory = path.resolve(process.env.GITHUB_WORKSPACE, core.getInput('working-directory'))
+	try {
+		const workingDirectory = path.resolve(process.env.GITHUB_WORKSPACE, core.getInput('working-directory'));
 
-    const [analyzeErrorCount, analyzeWarningCount] = await analyze(workingDirectory);
-    const formatWarningCount = await format(workingDirectory);
+		const [analyzeErrorCount, analyzeWarningCount] = await analyze(workingDirectory);
+		const formatWarningCount = await format(workingDirectory);
 
-    const issueCount = analyzeErrorCount + analyzeWarningCount + formatWarningCount;
-    const failOnWarnings = core.getInput('fail-on-warnings') === 'true';
-    const message = `${issueCount} issue${issueCount === 1 ? '' : 's'} found.`;
+		const issueCount = analyzeErrorCount + analyzeWarningCount + formatWarningCount;
+		const failOnWarnings = core.getInput('fail-on-warnings') === 'true';
+		const message = `${issueCount} issue${issueCount === 1 ? '' : 's'} found.`;
 
-    if (analyzeErrorCount > 0 || (failOnWarnings && issueCount > 0)) {
-      core.setFailed(message);
-    } else {
-      console.log(message);
-    }
-  } catch (error) {
-    core.setFailed(error.message);
-  }
+		if (analyzeErrorCount > 0 || (failOnWarnings && issueCount > 0)) {
+			core.setFailed(message);
+		} else {
+			console.log(message);
+		}
+	} catch (error) {
+		core.setFailed(error.message);
+	}
 }
 
 async function analyze(workingDirectory) {
-  let output = '';
+	let output = '';
 
-  const options = { cwd: workingDirectory };
-  options.listeners = {
-    stdout: (data) => {
-      output += data.toString();
-    },
-    stderr: (data) => {
-      output += data.toString();
-    }
-  };
+	const options = { cwd: workingDirectory };
+	options.listeners = {
+		stdout: (data) => {
+			output += data.toString();
+		},
+		stderr: (data) => {
+			output += data.toString();
+		},
+	};
 
-  const analysisOptionsFile = core.getInput('analysis-options-file');
-  const args = ['--format', 'machine'];
+	await exec.exec('dart', ['analyze'], options);
 
-  if (fs.existsSync(path.resolve(workingDirectory, analysisOptionsFile))) {
-    args.push('--options');
-    args.push(analysisOptionsFile);
-  }
+	let errorCount = 0;
+	let warningCount = 0;
+	const lines = output.trim().split(/\r?\n/);
+	const dataDelimiter = '|';
 
-  args.push('.');
+	for (const line of lines) {
+		if (!line.includes(dataDelimiter)) {
+			continue;
+		}
 
-  await exec.exec('dartanalyzer', args, options);
+		const lineData = line.split(dataDelimiter);
+		const lint = lineData[2];
+		const lintLowerCase = lint.toLowerCase();
+		const file = lineData[3].replace(workingDirectory, '');
+		const url =
+			lint === lintLowerCase ? `https://dart-lang.github.io/linter/lints/${lint}.html` : `https://dart.dev/tools/diagnostic-messages#${lintLowerCase}`;
+		const message = `file=${file},line=${lineData[4]},col=${lineData[5]}::${lineData[7]} For more details, see ${url}`;
 
-  let errorCount = 0;
-  let warningCount = 0;
-  const lines = output.trim().split(/\r?\n/);
-  const dataDelimiter = '|';
+		if (lineData[0] === 'ERROR') {
+			console.log(`::error ${message}`);
+			errorCount++;
+		} else {
+			console.log(`::warning ${message}`);
+			warningCount++;
+		}
+	}
 
-  for (const line of lines) {
-    if (!line.includes(dataDelimiter)) {
-      continue;
-    }
-
-    const lineData = line.split(dataDelimiter);
-    const lint = lineData[2];
-    const lintLowerCase = lint.toLowerCase();
-    const file = lineData[3].replace(workingDirectory, '');
-    const url = lint === lintLowerCase
-      ? `https://dart-lang.github.io/linter/lints/${lint}.html`
-      : `https://dart.dev/tools/diagnostic-messages#${lintLowerCase}`
-    const message = `file=${file},line=${lineData[4]},col=${lineData[5]}::${lineData[7]} For more details, see ${url}`;
-
-    if (lineData[0] === 'ERROR') {
-      console.log(`::error ${message}`);
-      errorCount++;
-    } else {
-      console.log(`::warning ${message}`);
-      warningCount++;
-    }
-  }
-
-  return [errorCount, warningCount];
+	return [errorCount, warningCount];
 }
 
 async function format(workingDirectory) {
-  let output = '';
+	let output = '';
 
-  const options = { cwd: workingDirectory };
-  options.listeners = {
-    stdout: (data) => {
-      output += data.toString();
-    },
-    stderr: (data) => {
-      output += data.toString();
-    }
-  };
+	const options = { cwd: workingDirectory };
+	options.listeners = {
+		stdout: (data) => {
+			output += data.toString();
+		},
+		stderr: (data) => {
+			output += data.toString();
+		},
+	};
 
-  const args = [];
-  const lineLength = core.getInput('line-length');
+	const args = ['format'];
+	const lineLength = core.getInput('line-length');
 
-  if (lineLength) {
-    args.push('--line-length');
-    args.push(lineLength);
-  }
+	if (lineLength) {
+		args.push('--line-length');
+		args.push(lineLength);
+	}
 
-  args.push('--dry-run');
-  args.push('.');
+	args.push('--dry-run');
+	args.push('.');
 
-  await exec.exec('dartfmt', args, options);
+	await exec.exec('dart', args, options);
 
-  let warningCount = 0;
-  const lines = output.trim().split(/\r?\n/);
+	let warningCount = 0;
+	const lines = output.trim().split(/\r?\n/);
 
-  for (const line of lines) {
-    if (!line.endsWith('.dart')) continue;
+	for (const line of lines) {
+		if (!line.endsWith('.dart')) continue;
 
-    console.log(`::warning file=${line}::Invalid format. For more details, see https://dart.dev/guides/language/effective-dart/style#formatting`);
-    warningCount++;
-  }
+		console.log(`::warning file=${line}::Invalid format. For more details, see https://dart.dev/guides/language/effective-dart/style#formatting`);
+		warningCount++;
+	}
 
-  return warningCount;
+	return warningCount;
 }
 
 run();
